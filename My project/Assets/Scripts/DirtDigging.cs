ba4c7f510c5cfcda1424ad677dbb51b693219a1a
Ambersae <1031107@student.sae.edu.au>
2025-11-20 13:22:31 +1000

    You can plant plants on dirt
    IT WORKS LES GOOO

--- My project/Assets/Scripts/DirtDigging.cs
+++ My project/Assets/Scripts/DirtDigging.cs
@@ -6,20 +6,41 @@ public class DirtDigging : MonoBehaviour, IInteractable
     private Renderer rend;
     public Material Dirt;
     public Material Grass;
+    public GameObject Plant;
+    public bool CheckDirt;
+    public bool PlantIsPlanted;
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.material = Grass;
+        CheckDirt = false;
+        PlantIsPlanted = false;
     }
 
     public void InteractLeftClick()
     {
         Debug.Log("tileInteracted");
-        rend.material = Dirt;
+        
+        if(CheckDirt == true)
+        {
+            Debug.Log("PlantCanBePlanted");
+            Instantiate(Plant, transform.position, Quaternion.identity);
+            PlantIsPlanted = true;
+        }
+        else
+        {
+            rend.material = Dirt;
+            CheckDirt = true;
+        }
     }
     public void InteractRightClick()
     {
-        Debug.Log("tileInteracted");
-        rend.material = Grass;
+        if (PlantIsPlanted == false)
+        {
+            Debug.Log("tileInteracted");
+            rend.material = Grass;
+            CheckDirt = false;
+        }
+        
     }
 }
