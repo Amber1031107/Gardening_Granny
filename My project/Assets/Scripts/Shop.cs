0257497893292a13b777264b0c5a8b1beb0d70b5
FBI_Hotbox\Amber <1031107@student.sae.edu.au>
2026-03-07 10:15:20 +1000

    shop

--- My project/Assets/Scripts/Shop.cs
+++ My project/Assets/Scripts/Shop.cs
@@ -12,6 +12,13 @@ public class Shop : MonoBehaviour, IInteractable
     public GameObject PlantsShop;
     public TextMeshProUGUI moneyText;
 
+    public Button buybutton1;
+    public Button buybutton2;
+    public Button buybutton3;
+    public Button buybutton4;
+    public Button buybutton5;
+    public Button buybutton6;
+
     private int moneyAmount;
     public int MoneyAmount
     {
@@ -25,7 +32,7 @@ public class Shop : MonoBehaviour, IInteractable
 
     void Start()
     {
-        MoneyAmount = 1000;
+        MoneyAmount = 100;
         storePrefab.SetActive(false);
         TrapsShop.SetActive(false);
         PlantsShop.SetActive(false);
@@ -47,10 +54,64 @@ public class Shop : MonoBehaviour, IInteractable
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-       // if (moneyAmount > 30)
-      //  {
-//
-      //  }
+        if (moneyAmount < 30)
+        {
+            buybutton6.interactable = false;
+        }
+        else
+        {
+            buybutton6.interactable = true;
+
+        }
+        if (moneyAmount < 25)
+        {
+            buybutton5.interactable = false;
+        }
+        else
+        {
+            buybutton5.interactable = true;
+
+        }
+        if (moneyAmount < 20)
+        {
+            buybutton4.interactable = false;
+        }
+        else
+        {
+
+            buybutton4.interactable = true;
+
+        }
+        if (moneyAmount < 15)
+        {
+            buybutton3.interactable = false;
+        }
+        else
+        {
+
+            buybutton3.interactable = true;
+
+        }
+        if (moneyAmount < 10)
+        {
+            buybutton2.interactable = false;
+        }
+        else
+        {
+
+            buybutton2.interactable = true;
+
+        }
+        if (moneyAmount < 5)
+        {
+            buybutton1.interactable = false;
+        }
+        else
+        {
+
+            buybutton1.interactable = true;
+
+        }
     }
     public void TrapsButton()
     {
@@ -72,37 +133,52 @@ public class Shop : MonoBehaviour, IInteractable
     }
     public void InteractRightClick()
     {
-        throw new System.NotImplementedException();
+        // throw new System.NotImplementedException();
+        moneyAmount = moneyAmount + 20;
+        UpdateMoneyUI();
     }
 
     public void BuyPlant1()
     {
         moneyAmount = moneyAmount - 5;
         UpdateMoneyUI();
+        
+        
     }
     public void BuyPlant2()
     {
         moneyAmount = moneyAmount - 10;
         UpdateMoneyUI();
+        
+        
     }
     public void BuyPlant3()
     {
         moneyAmount = moneyAmount - 15;
         UpdateMoneyUI();
+        
+        
     }
     public void BuyPlant4()
     {
         moneyAmount = moneyAmount - 20;
         UpdateMoneyUI();
+        
+        
     }
     public void BuyPlant5()
     {
         moneyAmount = moneyAmount - 25;
         UpdateMoneyUI();
+
+        
+        
     }
     public void BuyPlant6()
     {
         moneyAmount = moneyAmount - 30;
         UpdateMoneyUI();
+        
+        
     }
 }
