51a0145a99d003432bb2baa05185cc8435c2063d
CalmBigBoss25 <cameronstuart04@gmail.com>
2026-03-09 19:05:58 +1000

    UptoDate Wwise
    Phil's Progress has been restored, now just need to implement

--- My project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ My project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID TEST = 3157003241U;
+        static const AkUniqueID TEST_EVENT = 4258653052U;
     } // namespace EVENTS
 
     namespace BANKS
